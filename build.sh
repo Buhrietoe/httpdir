3a82fccc79bb02ae8eae825e5b4463f604e001a5
Jason Gardner <buhrietoe@gmail.com>
2017-07-13 16:59:21 -0500

    Added logging

--- build.sh
+++ build.sh
@@ -5,27 +5,27 @@ echo Building for all architectures
 echo Linux 32bit
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags '-w -s' -o target/httpdir32 .
 file target/httpdir32
-
+echo
 echo Linux 64bit
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -o target/httpdir .
 file target/httpdir
-
+echo
 echo Linux Arm 32bit
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags '-w -s' -o target/httpdir-arm .
 file target/httpdir-arm
-
+echo
 echo Linux Arm 64bit
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags '-w -s' -o target/httpdir-arm64 .
 file target/httpdir-arm64
-
+echo
 echo Windows 32bit
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o target/httpdir.exe .
 file target/httpdir.exe
-
+echo
 echo Windows 64bit
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o target/httpdir64.exe .
 file target/httpdir64.exe
-
+echo
 echo OSX 64bit
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o target/httpdir-osx .
 file target/httpdir-osx
--- main.go
+++ main.go
@@ -6,11 +6,19 @@ import (
 	"os"
 )
 
+// function logger logs all requests
+func logger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	if len(os.Args) > 1 {
 		log.Printf("Serving from: %s", os.Args[1])
-		panic(http.ListenAndServe(os.Args[1], http.FileServer(http.Dir("./"))))
+		panic(http.ListenAndServe(os.Args[1], logger(http.FileServer(http.Dir("./")))))
 	} else {
 		log.Fatalln("Please specify address:port to listen from")
 	}

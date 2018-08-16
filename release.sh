1c76c8d82c7c67781d920bb09e19112143f314ce
Jason Gardner <buhrietoe@gmail.com>
2018-08-16 11:02:27 -0500

    tidy release script. add mipsle build

--- release.sh
+++ release.sh
@@ -3,31 +3,42 @@
 echo Building for all architectures
 
 echo Linux 32bit
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags '-w -s' -o target/httpdir32 .
-file target/httpdir32
+TARGET=target/linux-32/httpdir
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags '-w -s' -o $TARGET .
+file $TARGET
 echo
 echo Linux 64bit
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -o target/httpdir .
-file target/httpdir
+TARGET=target/linux-64/httpdir
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -o $TARGET .
+file $TARGET
 echo
 echo Linux Arm 32bit
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags '-w -s' -o target/httpdir-arm .
-file target/httpdir-arm
+TARGET=target/linux-arm-32/httpdir
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags '-w -s' -o $TARGET .
+file $TARGET
 echo
 echo Linux Arm 64bit
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags '-w -s' -o target/httpdir-arm64 .
-file target/httpdir-arm64
+TARGET=target/linux-arm-64/httpdir
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags '-w -s' -o $TARGET .
+file $TARGET
+echo
+echo Linux MIPSLE 32bit
+TARGET=target/linux-mipsle-32/httpdir
+CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags '-w -s' -o $TARGET .
+file $TARGET
 echo
 echo Windows 32bit
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o target/httpdir.exe .
-file target/httpdir.exe
+TARGET=target/linux-windows-32/httpdir.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o $TARGET .
+file $TARGET
 echo
 echo Windows 64bit
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o target/httpdir64.exe .
-file target/httpdir64.exe
+TARGET=target/linux-windows-64/httpdir.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $TARGET .
+file $TARGET
 echo
 echo OSX 64bit
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o target/httpdir-osx .
-file target/httpdir-osx
-
+TARGET=target/linux-osx-64/httpdir
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $TARGET .
+file $TARGET
 
